175068af640422b63d090f8d227a9daf91693581
Jens-Falco Marecek <falcomarecek@gmail.com>
2022-09-26 15:36:58 +0200

    Initial Commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  <>
+    <h1>Book Libary</h1>
+    <p>All Books:</p>
+  </>
+  )
+}
+
+export default App;
